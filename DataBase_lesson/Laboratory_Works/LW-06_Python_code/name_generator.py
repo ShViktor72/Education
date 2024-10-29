de5ed21ab89fd4bde60a17f38b799c7501d4c849
VVV <vvv@gmail.com>
2024-10-29 10:24:11 +0500

    add data

--- DataBase_lesson/Laboratory_Works/LW-06_Python_code/name_generator.py
+++ DataBase_lesson/Laboratory_Works/LW-06_Python_code/name_generator.py
@@ -1,59 +1,15 @@
-import pymysql
-from faker import Faker
-import time
+# encoding_converter.py
 
-connection = pymysql.connect(
-    host='127.0.0.1',
-    port=3306,
-    user='root',
-    password='1234',
-    database='testdb',
-    cursorclass=pymysql.cursors.DictCursor
-)
+import codecs
 
-start_time = time.time()
+input_file = 'C:\\Users\\User\\Desktop\\College2024\\DataBase\\data\\pilevin.txt'  # Укажите путь к вашему файлу
+output_file = 'C:\\Users\\User\\Desktop\\College2024\\DataBase\\data\\pilevin_utf8.txt'  # Путь для сохранения преобразованного файла
 
-# Генерация списка имен
-faker = Faker("ru_RU")
-names = [faker.name() for i in range(100000)]
+# Читаем файл с одной кодировкой и сохраняем в другой
+with codecs.open(input_file, 'r', encoding='windows-1251') as f:
+    content = f.read()
 
-names_surnames_list = []
-for user in names:
-    split_name = user.split()
-    names_surnames_list.append([split_name[0], split_name[1]])  # Имя и Фамилия
+with codecs.open(output_file, 'w', encoding='utf-8') as f:
+    f.write(content)
 
-try:
-    with connection.cursor() as cursor:
-        sql = "INSERT INTO users (name, surname) VALUES (%s, %s)"
-        values = [(user_data[0], user_data[1]) for user_data in names_surnames_list]
-        cursor.executemany(sql, values)
-
-    # Подтверждение транзакции
-    connection.commit()
-
-except pymysql.MySQLError as err:
-    print(f"Ошибка при выполнении транзакции: {err}")
-    connection.rollback()
-
-finally:
-    # Закрытие соединения
-    connection.close()
-
-end_time = time.time()
-print("Время выполнения:", end_time - start_time, "секунд")
-
-
-"""
-%s	Строка	Универсальный заполнитель для строк.
-
-Основные методы faker:
-Генерация персональных данных
-name(): Генерирует случайное имя. Например, faker.name() может вернуть "Иван Петров".
-last_name(): Генерирует случайную фамилию.
-first_name(): Генерирует случайное имя.
-address(): Генерирует полный адрес, включая улицу, город, штат и т.д.
-city(): Генерирует название города.
-country(): Генерирует название страны.
-email(): Генерирует случайный email-адрес, комбинируя имя и фамилию с популярными доменами.
-phone_number(): Генерирует случайный номер телефона в определенном формате.
-"""
+print("Файл успешно преобразован.")
