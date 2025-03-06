0b6940e2bcfeb211da1e0696bd6c38266c4a6ff1
ShViktor <colledge20education23@gmail.com>
2025-03-06 08:40:17 +0500

    fix lw8

--- CSharp_lesson_2/08_HttpClient/HttpJsonBTC/HttpJsonBTC/obj/Debug/net8.0-windows/HttpJsonBTC.AssemblyInfo.cs
+++ CSharp_lesson_2/08_HttpClient/HttpJsonBTC/HttpJsonBTC/obj/Debug/net8.0-windows/HttpJsonBTC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HttpJsonBTC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09acbd243aef81a18dbd0ec98aa1a14bc5ff70e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("HttpJsonBTC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HttpJsonBTC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
